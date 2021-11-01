4c754abbed69dc25add635da71b7a13bcf46b104
YevaUsatova <yeva.usatova@gmail.com>
2021-10-31 23:46:54 -0400

    addons

--- animal-shelter-fe/src/components/AddAnimal.jsx
+++ animal-shelter-fe/src/components/AddAnimal.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react"
+import "./AnimalsForm.css"
 
 export default function AddAnimal({addAnimal}){
     const [form, setForm]= useState({
@@ -6,10 +7,27 @@ export default function AddAnimal({addAnimal}){
         species: "",
         age: 0
     })
+
+    const handleChange = (e) =>{
+        setForm ((prev) => {
+            return{
+                ...prev,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        addAnimal(form)
+    }
     return (
         <div>
-            <form>
-
+            <form onSubmit={handleSubmit}>
+                <input className= "animalsForm" name="name" onChange = {handleChange} type="text" value = {form["name"]}></input>
+                <input className= "animalsForm" name= "species" onChange = {handleChange} type="text" value = {form["species"]}></input>
+                <input className= "animalsForm" name="age" onChange = {handleChange} type="integer" value = {form["age"]}></input>
+                <button type="submit">Save me</button>
             </form>  
         </div>
     )
