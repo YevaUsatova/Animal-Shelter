0d8904283a8dddd9784d68b66288ac8fdb01848b
YevaUsatova <yeva.usatova@gmail.com>
2021-11-02 23:42:39 -0400

    post/delete

--- animal-shelter-api/app/controllers/animals_controller.rb
+++ animal-shelter-api/app/controllers/animals_controller.rb
@@ -3,9 +3,20 @@ class AnimalsController < ApplicationController
         Animal.all.to_json
     end
 
-    post "./animal" do
+    post "/animals" do
         animals = Animal.find_or_create_by(name: params[:name], species: params[:species], age: params[:age])
+        animals.to_json
     end
 
+    get "/animals/:id" do 
+        animals = Animal.find(params[:id])
+        animals.to_json(:include => :shelter, :except => :shelter_id)
+    end
+
+    delete "/animals/:id" do 
+        animals = Animal.find(params[:id])
+        animals.delete
+
+    end 
     
 end
